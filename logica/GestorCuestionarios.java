509cce30a2df494ff772942aa8be94e2debd031a
Eggop92 <epuerta001@ikasle.ehu.es>
2014-04-28 20:52:21 +0200

    añadida la parte mae de la clase gestorCuestionario.

--- logica/GestorCuestionarios.java
+++ logica/GestorCuestionarios.java
@@ -1,5 +1,17 @@
 package logica;
 
 public class GestorCuestionarios {
+	
+	private static GestorCuestionarios mgc;
+	
+	private GestorCuestionarios(){}
+	
+	public static GestorCuestionarios getGestorCuestionarios(){
+		if(mgc == null){
+			mgc = new GestorCuestionarios();
+		}
+		return mgc;
+	}
 
+	
 }
