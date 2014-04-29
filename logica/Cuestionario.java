c88a98b49ad6c4bb87043f314b374ecfc78dbbf9
Bego <bjusto002@ikasle.ehu.es>
2014-04-29 15:24:51 +0200

    Modificación de Cuestionario, Pregunta.

--- logica/Cuestionario.java
+++ logica/Cuestionario.java
@@ -6,12 +6,15 @@ public class Cuestionario {
 	
 	private String nombre;
 	private int id;
-	private int numPreguntas;
+	//private int numPreguntas;
+	private LinkedList<Pregunta> listaPreguntas; 
 
-	public Cuestionario(int pId, String pNombre, int pNumPreg) {
+	public Cuestionario(int pId, String pNombre) {
 		id = pId;
 		nombre = pNombre;
-		numPreguntas = pNumPreg;
+		//numPreguntas = pNumPreg;
+		this.cargarPreguntas();
+		
 	}
 
 	public String getNombre() {
@@ -21,6 +24,20 @@ public class Cuestionario {
 	public LinkedList<Pregunta> getPreguntas() {
 		// TODO EGOITZ
 		return null;
+	}
+	
+	private int getNumPreguntas(){
+		return listaPreguntas.size();
+	}
+	
+	public LinkedList<Pregunta> cargarPreguntas(){
+		// cargar la base de datos.
+		return this.listaPreguntas;
+	}
+	
+	public void anadePregunta(Pregunta pPregunta){
+		this.listaPreguntas.add(pPregunta);
+		
 	}
 
 	@Override
--- logica/Pregunta.java
+++ logica/Pregunta.java
@@ -4,6 +4,23 @@ import java.util.LinkedList;
 
 public class Pregunta {
 
+	private int id;
+	private String titulo;
+	private String respuesta;
+	private LinkedList<String> listaRespuestas;
+
+	public Pregunta(int pId){
+		this.id = pId;
+	}
+
+	private String getTitulo(){
+		return this.titulo;
+	}
+
+	public LinkedList<String> cargarRespuestas(){
+		return this.listaRespuestas;
+	}
+
 	public LinkedList<String> getRespuestas() {
 		// TODO EGOITZ
 		return null;
