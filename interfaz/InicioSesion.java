d427cfa50d8a583a68bac24c2d24002cacbd9955
David Espino <davidespinogomez@gmail.com>
2014-04-30 17:49:24 +0200

    Añadido los metodos para iniciar sesión y el label de error

--- interfaz/InicioSesion.java
+++ interfaz/InicioSesion.java
@@ -17,8 +17,13 @@ import javax.swing.JPanel;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class InicioSesion {
+import logica.GestorLogin;
+import java.awt.Color;
+
+public class InicioSesion extends JFrame {
 
 	private JFrame frmLogicgroup;
 	private JTextField textFieldUsuario;
@@ -53,25 +58,16 @@ public class InicioSesion {
 	private void initialize() {
 		frmLogicgroup = new JFrame();
 		frmLogicgroup.setTitle("LogicGroup");
-		frmLogicgroup.setBounds(100, 100, 335, 191);
+		frmLogicgroup.setBounds(100, 100, 335, 209);
 		frmLogicgroup.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		//Panels
 		JPanel panel = new JPanel();
 		frmLogicgroup.getContentPane().add(panel, BorderLayout.NORTH);
-		
-		JLabel lblInicioDeSesin = new JLabel("Inicio de sesi\u00F3n");
-		lblInicioDeSesin.setFont(new Font("Tahoma", Font.BOLD, 16));
-		panel.add(lblInicioDeSesin);
 		
 		JPanel panel_1 = new JPanel();
 		frmLogicgroup.getContentPane().add(panel_1, BorderLayout.SOUTH);
 		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
-		JButton btnAceptar = new JButton("Aceptar");
-		panel_1.add(btnAceptar);
-		
-		JButton btnSalir = new JButton("   Salir   ");
-		panel_1.add(btnSalir);
 		
 		JPanel panel_2 = new JPanel();
 		frmLogicgroup.getContentPane().add(panel_2, BorderLayout.CENTER);
@@ -80,6 +76,17 @@ public class InicioSesion {
 		JPanel panel_3 = new JPanel();
 		panel_2.add(panel_3, BorderLayout.NORTH);
 		
+		JPanel panel_4 = new JPanel();
+		panel_2.add(panel_4, BorderLayout.CENTER);
+		
+		JPanel panel_5 = new JPanel();
+		panel_2.add(panel_5, BorderLayout.SOUTH);
+		
+		//Labels and textfields
+		JLabel lblInicioDeSesin = new JLabel("Inicio de sesi\u00F3n");
+		lblInicioDeSesin.setFont(new Font("Tahoma", Font.BOLD, 16));
+		panel.add(lblInicioDeSesin);
+		
 		JLabel lblUsuario = new JLabel("Usuario:     ");
 		panel_3.add(lblUsuario);
 		
@@ -87,8 +94,6 @@ public class InicioSesion {
 		panel_3.add(textFieldUsuario);
 		textFieldUsuario.setColumns(15);
 		
-		JPanel panel_4 = new JPanel();
-		panel_2.add(panel_4, BorderLayout.CENTER);
 		
 		JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
 		panel_4.add(lblContrasea);
@@ -96,5 +101,61 @@ public class InicioSesion {
 		passwordFieldContrasena = new JPasswordField();
 		panel_4.add(passwordFieldContrasena);
 		passwordFieldContrasena.setColumns(15);
+		
+		
+		final JLabel lblError = new JLabel("");
+		lblError.setForeground(Color.RED);
+		panel_5.add(lblError);
+		
+		
+		//Buttons
+		JButton btnAceptar = new JButton("Aceptar");
+		btnAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String usuario = textFieldUsuario.getText();
+				if (usuario.isEmpty()){
+					lblError.setText("Error Usuario vacio");
+				}
+				else if (passwordFieldContrasena.getPassword().length==0) {
+					lblError.setText("Error contraseña vacia");
+				}
+				else {
+					String tipo = GestorLogin.getMiGestorLogin().login(usuario, passwordFieldContrasena.getPassword().toString());
+					if (tipo == "fallo"){
+						lblError.setText("Contraseña o usuario incorrectos");
+					}
+					else if (tipo == "rellenador") {
+						RellenarCuestionario frame = new RellenarCuestionario(usuario);
+						frame.setVisible(true);
+						dispose();
+					}
+					else if (tipo == "contestador") {
+						ConfiguracionCuestionario frame = new ConfiguracionCuestionario(usuario);
+						frame.setVisible(true);
+						dispose();
+					}
+					else if (tipo == "consultor") {
+						ConsultarCuestionario frame = new ConsultarCuestionario(usuario);
+						frame.setVisible(true);
+						dispose();
+					}
+					else{
+						lblError.setText("Error desconocido");
+					}
+				}
+			}
+		});
+		panel_1.add(btnAceptar);
+		
+		
+		
+		
+		JButton btnSalir = new JButton("   Salir   ");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		panel_1.add(btnSalir);
 	}
 }
